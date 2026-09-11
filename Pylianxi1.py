4d5313614a1b46aadb2cda2df57425a1b88e2777
ElE Jack <jack@ElEdeMacBook-Air.local>
2026-09-12 05:30:34 +0800

    首次提交：上传Python练习文件

---
+++ Pylianxi1.py
@@ -0,0 +1,6 @@
+money_num = 50 # 直接设置初始余额为50元
+print("初始余额为：", money_num) # 输出初始余额
+money_num = money_num - 10 # 购买冰淇淋后余额为初始余额减去10元
+print("购买冰淇淋后余额为：", money_num) # 购买冰淇淋后余额为初始余额减去10元
+money_num = money_num - 5 # 购买可乐后余额为购买冰淇淋后余额减去5元
+print("购买可乐后余额为：", money_num) # 购买可乐后余额为购买冰淇淋后余额减去5元
